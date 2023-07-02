cb9335a81c3f4f47fba9b4481b3d1aaaf1623d52
Fernanda Giannini <elcambionv@gmail.com>
2023-07-02 18:31:28 -0300

    Change mostrar cadena

--- ejercicio1C11.js
+++ ejercicio1C11.js
@@ -6,10 +6,10 @@ let upperLimitOdd = 10
 console.log ("NUMEROS PARES")
 for (lowerLimitEven; lowerLimitEven <= upperLimitEven; lowerLimitEven++){
     if (lowerLimitEven % 2 == 0)
-    console.log("El número " + lowerLimitEven + " " + "es par")
+    console.log(`El número ${lowerLimitEven} es par`)
 }
 console.log ("NUMEROS IMPARES")
 for (lowerLimitOdd; lowerLimitOdd <= upperLimitOdd; lowerLimitOdd++){
     if (lowerLimitOdd % 2 == !0)
-    console.log("El número " + lowerLimitOdd + " " + "es impar")
+    console.log(`El número ${lowerLimitOdd} es impar`)
 }
